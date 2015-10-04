ba58291e718c7e42dd294179813e5dac0f527b3e
Rashik <tuladhar.rashik@gmail.com>
2015-10-04 21:01:12 +0545

    lights tables added in db

--- db_ecommerce.sql
+++ db_ecommerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2015 at 02:33 PM
+-- Generation Time: Oct 04, 2015 at 05:15 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `tbl_aboutus` (
 --
 
 INSERT INTO `tbl_aboutus` (`id`, `description`, `createdby`, `createddate`) VALUES
-(1, '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32<br />\r\n<br />\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>\r\n', 'Rashik Tuladhar', '2015-09-20 12:55:50');
+(1, '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32<br />\r\n<br />\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>\r\n', 'Rashik Tuladhar', '2015-10-02 03:47:30');
 
 -- --------------------------------------------------------
 
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `tbl_category` (
   `description` varchar(255) NOT NULL,
   `createdby` varchar(100) NOT NULL,
   `createddate` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_category`
@@ -81,7 +81,78 @@ CREATE TABLE IF NOT EXISTS `tbl_category` (
 
 INSERT INTO `tbl_category` (`id`, `category`, `description`, `createdby`, `createddate`) VALUES
 (1, 'Chandlier', 'Chandlier Description', 'Rashik Tuladhar', '2015-09-20 05:04:58'),
-(2, 'Lights', 'lightsRashik', 'Rashik Tuladhar', '2015-09-20 05:52:31');
+(2, 'Lights', 'lightsRashik', 'Rashik Tuladhar', '2015-09-20 05:52:31'),
+(24, 'Plugs', 'Plugs', 'Rashik Tuladhar', '2015-09-21 10:27:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_products`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_products` (
+  `id` int(11) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `image` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `availability` int(11) NOT NULL,
+  `createdby` varchar(255) NOT NULL,
+  `createddate` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_products`
+--
+
+INSERT INTO `tbl_products` (`id`, `product`, `category`, `description`, `image`, `price`, `availability`, `createdby`, `createddate`) VALUES
+(6, 'Contrary ', 2, '<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&#39;t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn&#39;t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>\r\n', '', 2000, 1, 'Rashik Tuladhar', '2015-09-23 09:48:24'),
+(7, 'Lorem Ipsum', 1, '<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n', '', 1500, 0, 'Rashik Tuladhar', '2015-09-23 09:47:41'),
+(8, 'established ', 2, '<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.</p>\r\n\r\n<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>\r\n', '8746a5da963e2a9718f909e9e55edd05.jpg', 3000, 0, 'Rashik Tuladhar', '2015-09-23 09:49:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_slider`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_slider` (
+  `id` int(11) NOT NULL,
+  `sliderimage` varchar(255) DEFAULT NULL,
+  `sliderdesc` varchar(255) DEFAULT NULL,
+  `createdby` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL,
+  `isactive` varchar(5) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_slider`
+--
+
+INSERT INTO `tbl_slider` (`id`, `sliderimage`, `sliderdesc`, `createdby`, `createddate`, `isactive`) VALUES
+(5, 'b4bb4bd04ef99168ae28306ea7f03f1b.jpg', 'test slider image', 'Rashik Tuladhar', '2015-09-28 10:47:52', 'Y'),
+(6, '30d529932120fbcdcc0e4fa5cea9301f.jpg', 'test slider image2', 'Rashik Tuladhar', '2015-09-28 10:52:01', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_subscribers`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_subscribers` (
+  `id` int(11) NOT NULL,
+  `subscriberemail` varchar(255) DEFAULT NULL,
+  `ipaddress` varchar(255) DEFAULT NULL,
+  `createddate` datetime DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_subscribers`
+--
+
+INSERT INTO `tbl_subscribers` (`id`, `subscriberemail`, `ipaddress`, `createddate`) VALUES
+(1, 'tuladhar@gmail.com', '127.0.0.1', '2015-10-03 11:18:10');
 
 --
 -- Indexes for dumped tables
@@ -106,6 +177,24 @@ ALTER TABLE `tbl_category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_slider`
+--
+ALTER TABLE `tbl_slider`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_subscribers`
+--
+ALTER TABLE `tbl_subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -123,7 +212,22 @@ ALTER TABLE `tbl_applicationusers`
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `tbl_slider`
+--
+ALTER TABLE `tbl_slider`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `tbl_subscribers`
+--
+ALTER TABLE `tbl_subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
